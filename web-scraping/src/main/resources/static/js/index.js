ea8562bd46f7de9757bb2f1aa8b82741a3f7d837
Arjun N <53253167+Arjunarju717@users.noreply.github.com>
2020-10-12 19:18:34 +0530

    Loading Gif and Clean Up

--- web-scraping/src/main/resources/static/js/index.js
+++ web-scraping/src/main/resources/static/js/index.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
 	$("#scrapeEventData").click(function(e) {
+		$('#loading-image').show();
 		$.ajax({
 			url : '/eventDataScrapping',
 			type : 'GET',
 			dataType : 'json',
 			contentType : 'application/json',
 			success : function(result) {
+				$('#loading-image').hide();
 				alert(result.message);
 			},
 			error : function(error) {
+				$('#loading-image').hide();
 				alert(error.message);
 			}
-
 		});
 
 	});
